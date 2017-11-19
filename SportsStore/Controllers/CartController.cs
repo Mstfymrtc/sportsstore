9ad8b396fa4400f05ab65aceaf453bdb87780001
MUSTAFA YUMURTACI <mustafa.yumurtaci@hotmail.com>
2017-11-20 01:59:45 +0300

    created session extensions class

--- SportsStore/Controllers/CartController.cs
+++ SportsStore/Controllers/CartController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,6 +19,38 @@ namespace SportsStore.Controllers
         {
             repository = repo;
         }
+        public RedirectToActionResult AddToCart (int productId,string returnUrl) { 
+                  Product product = repository.Products.FirstOrDefault(x => x.ProductID == productId);
 
+            if (product!=null)
+            {
+                Cart cart = GetCart();
+                cart.AddItem(product, 1);
+                SaveCart(cart);
+
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectResult RemoveFromCart(int productId,string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            if (product!=null)
+            {
+                Cart cart = GetCart();
+                cart.RemoveLine(product);
+                SaveCart(cart);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();            return cart;
+        }
+        private void SaveCart(Cart cart)
+        {
+            HttpContext.Session.SetJson("Cart", cart);
+        }
     }
 }
