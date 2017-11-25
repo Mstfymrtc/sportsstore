ce44c54bcf4365f6d6af4a64fe6d12767a42f3b3
MUSTAFA YUMURTACI <mustafa.yumurtaci@hotmail.com>
2017-11-25 17:59:54 +0300

    //synnergy commit

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using SportsStore.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 
 namespace SportsStore
@@ -19,13 +20,14 @@ namespace SportsStore
             Configuration = configuration;
 
         public IConfiguration Configuration { get; }
-
+        //
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:SportStoreProducts:ConnectionString"]));
+            services.AddDbContext<AppIdentityDbContext>(options=>options.UseSqlServer(Configuration["Data:"]))
 
            
             services.AddMvc(); ////n
